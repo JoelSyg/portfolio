5b1eac55aea80bca4f9f6e701dee24c58e949f71
Joel <sygullajoel@gmail.com>
2024-08-05 13:50:37 +0200

    add initial hero-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
