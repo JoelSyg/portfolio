10edda21223b49f9d462ae36940bb6b007642aea
Joel <sygullajoel@gmail.com>
2024-08-07 19:06:55 +0200

    design/about-me and my-skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
@@ -8,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
+skills =
+[
+  {
+      "name": "Angular",
+      "image": "./assets/img/angular_icon.svg"
+  },
+  {
+      "name": "TypeScript",
+      "image": "./assets/img/ts_icon.svg"
+  },
+  {
+      "name": "JavaScript",
+      "image": "./assets/img/js_icon.svg"
+  },
+  {
+      "name": "HTML",
+      "image": "./assets/img/html_icon.svg"
+  },
+  {
+      "name": "CSS",
+      "image": "./assets/img/css_icon.svg"
+  },
+  {
+      "name": "Firebase",
+      "image": "./assets/img/firebase_icon.svg"
+  },
+  {
+      "name": "Git",
+      "image": "./assets/img/git_icon.svg"
+  },
+  {
+      "name": "Scrum",
+      "image": "./assets/img/scrum_icon.svg"
+  },
+  {
+      "name": "Rest-Api",
+      "image": "./assets/img/api_icon.svg"
+  },
+  {
+      "name": "Material Design",
+      "image": "./assets/img/material_icon.svg"
+  }
+]
+
+
 
 }
