43dc86838fe11d0bc012d112391084b916563780
Joel <sygullajoel@gmail.com>
2024-08-09 11:38:30 +0200

    design/portfolio-section projects

--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
   imports: [],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent {
-
+  @Input() project!: {
+    title: string;
+    technologies: string[];
+    description: string;
+    livetestLink: string;
+    githubLink: string;
+    imageUrl: string;
+  };
 }
