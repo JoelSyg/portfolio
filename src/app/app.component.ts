981e6ad5dfa077452fc0bbced06c3aa89e251b8f
Joel <sygullajoel@gmail.com>
2024-08-09 11:37:38 +0200

    design/say-hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,14 @@ import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { SayHiComponent } from './say-hi/say-hi.component';
+import { FooterComponent } from "./footer/footer.component";
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SayHiComponent, FooterComponent, ReferencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
