2558eaf0d0cb3f67432eeb1efc9d539c2be6d23a
Joel <sygullajoel@gmail.com>
2024-08-12 20:09:07 +0200

    add burger menu design and functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,57 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  menuOpen = false;
+  imgSource = "./assets/img/menu_icon.svg";
+
+  transitionImagesOpen = [
+    "./assets/img/menu_transition3.svg",
+    "./assets/img/menu_transition2.svg",
+    "./assets/img/menu_transition1.svg"
+  ];
+
+  transitionImagesClose = [
+    "./assets/img/menu_transition1.svg",
+    "./assets/img/menu_transition2.svg",
+    "./assets/img/menu_transition3.svg"
+  ];
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      this.playOpenAnimation();
+    } else {
+      this.playCloseAnimation();
+    }
+  }
+
+  playOpenAnimation() {
+    let index = 0;
+    const interval = setInterval(() => {
+      this.imgSource = this.transitionImagesOpen[index];
+      index++;
+      if (index > this.transitionImagesOpen.length) {
+        clearInterval(interval);
+        this.imgSource = "./assets/img/x_icon.svg";
+      }
+    }, 40);
+  }
+
+  playCloseAnimation() {
+    let index = 0;
+    const interval = setInterval(() => {
+      this.imgSource = this.transitionImagesClose[index];
+      index++;
+      if (index > this.transitionImagesClose.length) {
+        clearInterval(interval);
+        this.imgSource = "./assets/img/menu_icon.svg";
+      }
+    }, 40);
+  }
 }
+
