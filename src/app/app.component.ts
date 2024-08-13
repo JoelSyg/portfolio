77d41652a58b8bf7227b65b6ee3e6f3b26c2d689
Joel <sygullajoel@gmail.com>
2024-08-13 15:29:49 +0200

    move header and footer components to shared folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { SayHiComponent } from './say-hi/say-hi.component';
-import { FooterComponent } from "./footer/footer.component";
 import { ReferencesComponent } from './references/references.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SayHiComponent, FooterComponent, ReferencesComponent],
+  imports: [CommonModule, RouterOutlet, HeroSectionComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, SayHiComponent, ReferencesComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
