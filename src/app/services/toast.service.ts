d5057eb20681e571edfe6fe3cd25c5bbf5c1e34a
Joel <sygullajoel@gmail.com>
2024-09-02 21:25:18 +0200

    add toast message and translation service

---
+++ src/app/services/toast.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  private toastElement: HTMLElement | null = null;
+
+  show(message: string, duration: number = 3000) {
+    if (!this.toastElement) {
+      this.toastElement = document.createElement('div');
+      this.toastElement.className = 'toast-message';
+      document.body.appendChild(this.toastElement);
+    }
+    this.toastElement.innerText = message;
+    this.toastElement.classList.add('show');
+
+    setTimeout(() => {
+      if (this.toastElement) {
+        this.toastElement.classList.remove('show');
+      }
+    }, duration);
+  }
+
+}
